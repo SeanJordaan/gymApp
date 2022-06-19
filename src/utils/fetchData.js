3f3cf1bcc0aaa22c6bdbbd09d3d8287ec8fa40f0
Sean <jordaansean7@gmail.com>
2022-06-19 09:58:49 +0200

    first commit

---
+++ src/utils/fetchData.js
@@ -0,0 +1,22 @@
+export const exerciseOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': '264f4dfd9bmshdef9f3aa45ec04ap1ecbb8jsn14d1b799aa21',
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+  },
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': 'f0021db587msh781fb1cbef39856p11c183jsn45521d5d1c85',
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
